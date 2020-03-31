5756504657769e8f02fdcb592eedce381732999f
adrianz <adrianz@google.com>
2020-03-31 09:23:00 +0200

    wip

--- kokoro/build_script.sh
+++ kokoro/build_script.sh
@@ -2,3 +2,12 @@
 echo "hello world"
 find .
 
+if [ -n "$KOKORO_ARTIFACTS_DIR" ]; then
+  cd "${KOKORO_ARTIFACTS_DIR}/github/hello-world"
+  # Need to use the . to run in the same shell to inherit exports
+  . kokoro/setup.sh
+fi
+
+mkdir -p test_logs
+cp kokoro/log.xml > test_logs
+
